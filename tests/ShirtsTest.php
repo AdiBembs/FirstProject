d766d527a0a2cdb4e4cba86d873e462ed00277c6
Alondra Avila <aa2545@njit.edu>
2019-02-28 11:45:53 -0500

    Step 1: created ShirtsTest PHP file to test the array of shirt colors

---
+++ tests/ShirtsTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lola
+ * Date: 2/28/2019
+ * Time: 11:41 AM
+ */
+declare(strict_types=1);
+
+use FirstProjectmaster\Framework\TestCase;
+
+final class ShirtTest extends TestCase
+{
+
+    public function testArray()
+    {
+
+        $shirt = new Gucci();
+
+        $ShirtColors = $shirt->shirtColorColor();
+        $this->assertArrayHasKey(key: 'one', $ShirtColors);
+        $this->assertArrayHasKey(key: 'two', $ShirtColors);
+        $this->assertArrayHasKey(key: 'three', $ShirtColors);
+
+    }
+
+    
