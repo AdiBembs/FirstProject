5f5565a4e6ce31c17f4e1a8211c6bde619f55b24
Aditya <ab946@njit.edu>
2019-03-13 16:34:20 -0400

    Modified index.php to include html syntax, and tried to test if the table outputs. The idea behind initially use html syntax instead of bootstrap is to create a skeleton of the code rather than diving in immediately

--- src/Main.php
+++ src/Main.php
@@ -6,13 +6,13 @@
  * Time: 12:07 PM
  */
 
-Main::start(data.csv);
+//Main::start("../Data/data.csv");
 
 class Main
 {
     static public function start($filename) {
 
-        $records = csv::getRecords($filename);
+        $records = extractCSV::getRecords($filename);
         $table = html::generateTable($records);
 
 
