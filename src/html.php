f3230157191f56e72b3540cbf15567dec2495c5a
Aditya <ab946@njit.edu>
2019-03-13 21:33:33 -0400

    Since the program was giving an error that the returnArray function could not be found, I modified the code to call the record class in the src directory. The record class contained returnArray(), and this eliminated any errors

---
+++ src/html.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Aditya
+ * Date: 3/13/2019
+ * Time: 3:55 PM
+ */
+
+class html
+{
+    public static function generateTable($records) {
+
+        $count = 0;
+
+        foreach ($records as $record) {
+
+            if($count == 0) {
+
+                $array = $record->record::returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+
+            } else {
+                $array = $record->record::returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
+    }
+}
