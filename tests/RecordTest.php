328876e84b8d39164c50f6236faac89a70254e17
Alondra Avila <aa2545@njit.edu>
2019-03-14 00:12:09 -0400

    Edited RecordTest to test whether the construction of arrays exists, the arrays are returned, and the properties "name" and "value" were created within the record class exists.

--- tests/RecordTest.php
+++ tests/RecordTest.php
@@ -5,4 +5,26 @@
  * Date: 3/13/2019
  * Time: 6:28 PM
  */
+declare(strict_types=1);
+use FirstProject\Framework\TestCase;
 
+final class RecordTest extends TestCase
+{
+    public function testConstructArraysExists() {
+
+        $this->assertTrue(
+            method_exists(record::class, '_construct')
+        );
+    }
+
+    public function testReturnedArrays() {
+
+        $record = record::returnArray("...data/data.csv",'Main');
+        print_r($record);
+
+    }
+
+    public function testPropertyCreated() {
+        print_r('value', 'name');
+    }
+}
