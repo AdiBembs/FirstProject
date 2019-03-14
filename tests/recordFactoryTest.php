835534a1e3c234b3d8efe6334d020e767aa10c71
Alondra Avila <aa2545@njit.edu>
2019-03-13 23:44:40 -0400

    Edited recordFactoryTest to test whether the array creation within the recordFactory class exists.

--- tests/recordFactoryTest.php
+++ tests/recordFactoryTest.php
@@ -5,3 +5,23 @@
  * Date: 3/13/2019
  * Time: 6:29 PM
  */
+declare(strict_types=1);
+use FirstProject\Framework\TestCase;
+
+final class recordFactoryTest extends TestCase
+{
+
+    public function testCreatesArrayExists() {
+
+        $this->assertTrue(
+            method_exists(Main::class, 'recordFactory')
+        );
+    }
+
+    public function testReadCSVtoArray() {
+
+        $record = Main::recordFactory("Data/data.csv",'Shirts');
+        print_r($record);
+
+    }
+}
